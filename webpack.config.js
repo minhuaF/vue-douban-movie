57a4a9c64dbc3dc2412b3ac7b7a91e83980b4ec5
minhuaF <905102996@qq.com>
2017-06-16 17:59:48 +0800

    intial

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './dist',
+    publicPath: 'dist/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test : /\.vue$/,
+        loader: 'vue'
+      }
+    ]
+  }
+}
